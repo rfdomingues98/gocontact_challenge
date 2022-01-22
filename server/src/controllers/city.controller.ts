366ecc4c763c539f6bcc39bd71e5f5fd9c3c794b
Ricardo Domingues <rfdomingues98@gmail.com>
2022-01-22 19:38:39 +0000

    Optimized city list file. Modified city controller accordingly to city list.

--- server/src/controllers/city.controller.ts
+++ server/src/controllers/city.controller.ts
@@ -20,7 +20,12 @@ class CityController {
       const { name } = req.query;
       const data = (await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${process.env.API_KEY}`)).data;
 
-      const result = { temperature: data.main.temp, sunrise: data.sys.sunrise, sunset: data.sys.sunset };
+      const result = {
+        name: [data.name, data.sys.country].join(','),
+        temperature: data.main.temp,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+      };
 
       res.status(200).json(result);
     } catch (error) {
@@ -37,16 +42,19 @@ class CityController {
       const rawCities = await fs.readFile(path.resolve(__dirname, '../utils/city.list.min.json'), 'utf-8');
       let cities = JSON.parse(rawCities.toString());
       cities = cities.filter(city => namesArray.includes(city.name));
-      cities = cities.reduce((unique, item) => {
-        return unique.map(city => [city.name, city.state, city.country].join(',')).includes([item.name, item.state, item.country].join(','))
-          ? unique
-          : [...unique, item];
-      }, []);
       const ids = cities.map(city => city.id);
-      const data = (await axios.get(`https://api.openweathermap.org/data/2.5/group?id=${ids}&units=metric&appid=${process.env.API_KEY}`)).data;
+      const data = (await axios.get(`https://api.openweathermap.org/data/2.5/group?id=${ids.join(',')}&units=metric&appid=${process.env.API_KEY}`))
+        .data;
 
-      const result = data.list;
-      result.map(city => ({ name: city.name, temperature: city.main.temp, sunrise: city.sys.sunrise, sunset: city.sys.sunset }));
+      let result = data.list;
+      result = await result.map(city => {
+        return {
+          name: [city.name, city.sys.country].join(','),
+          temperature: city.main.temp,
+          sunrise: city.sys.sunrise,
+          sunset: city.sys.sunset,
+        };
+      });
 
       res.status(200).json(result);
     } catch (error) {
